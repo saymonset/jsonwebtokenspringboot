5a20f478c209bbf565179898b600d8b4aceea725
Simon Alberto Rodriguez Pacheco <saymon_set@hotmail.com>
2019-04-09 16:29:28 -0400

    Firs java service task with delegate

---
+++ src/main/java/com/service/BillAttachmentService.java
@@ -0,0 +1,23 @@
+package com.service;
+
+
+import org.flowable.engine.delegate.DelegateExecution;
+import org.flowable.engine.delegate.JavaDelegate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BillAttachmentService implements JavaDelegate{
+	Logger log = LoggerFactory.getLogger(this.getClass().getName());
+
+	@Override
+	public void execute(DelegateExecution execution) {
+		String executionId = execution.getProcessInstanceId();
+		
+		
+		 
+		log.info(execution.getVariable("format") + " = format-------------------------2---------------------------------executionId = " + executionId);
+
+		
+	}
+
+}
