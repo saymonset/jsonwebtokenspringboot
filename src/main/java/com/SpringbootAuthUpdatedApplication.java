02dcf2021e4c4b6c85bb0bc3c3d2beb13a6cadea
Simon Alberto Rodriguez Pacheco <saymon_set@hotmail.com>
2019-04-11 16:20:02 -0400

    Se corre el flujo sin security token baren

--- src/main/java/com/SpringbootAuthUpdatedApplication.java
+++ src/main/java/com/SpringbootAuthUpdatedApplication.java
@@ -4,7 +4,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import com.service.MyService;
 @SpringBootApplication
@@ -12,10 +11,7 @@ public class SpringbootAuthUpdatedApplication {
 
 
 
-	@Bean
-	public BCryptPasswordEncoder bCryptPasswordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootAuthUpdatedApplication.class, args);
 
